67da7a49e5f5059f93afab3cb3a72f88d3c2244a
leon <dkdlshs123@naver.com>
2025-12-05 17:32:45 +0900

    update: 테스트코드 추가

---
+++ CarrotTest/CarrotTestTests/CarrotTestTests.swift
@@ -0,0 +1,16 @@
+//
+//  CarrotTestTests.swift
+//  CarrotTestTests
+//
+//  Created by vision on 12/5/25.
+//
+
+import Testing
+
+struct CarrotTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
